396c7cd851d1892b6d419b2fdd93c0cbf4a457a3
kapta <chanzig@qq.com>
2015-09-20 00:54:22 +0800

    init

---
+++ mymvcpj/MvcApplication1/Common/EnumHelper.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace MvcApplication1.Common
+{
+    public static class EnumHelper
+    {
+        /// <summary>  
+        /// 扩展方法：根据枚举值得到相应的枚举定义字符串  
+        /// </summary>  
+        /// <param name="value"></param>  
+        /// <param name="enumType"></param>  
+        /// <returns></returns>  
+        public static String ToEnumString(int value, Type enumType)
+        {
+            NameValueCollection nvc = GetEnumStringFromEnumValue(enumType);
+            return nvc[value.ToString(CultureInfo.InvariantCulture)];
+        }
+
+        /// <summary>  
+        /// 根据枚举类型得到其所有的 值 与 枚举定义字符串 的集合  
+        /// </summary>  
+        /// <param name="enumType"></param>  
+        /// <returns></returns>  
+        public static NameValueCollection GetEnumStringFromEnumValue(Type enumType)
+        {
+            var nvc = new NameValueCollection();
+            FieldInfo[] fields = enumType.GetFields();
+            string strText = string.Empty;
+            string strValue = string.Empty;
+            foreach (FieldInfo field in fields)
+            {
+                if (field.FieldType.IsEnum)
+                {
+                    strValue = ((int)enumType.InvokeMember(field.Name, BindingFlags.GetField, null, null, null)).ToString();
+                    nvc.Add(strValue, field.Name);
+                }
+            }
+            return nvc;
+        }
+
+        /// <summary>  
+        /// 扩展方法：根据枚举值得到属性Description中的描述, 如果没有定义此属性则返回空串  
+        /// </summary>  
+        /// <param name="value"></param>  
+        /// <param name="enumType"></param>  
+        /// <returns></returns>  
+        public static String ToEnumDescriptionString(int value, Type enumType)
+        {
+            NameValueCollection nvc = GetNVCFromEnumValue(enumType);
+            return nvc[value.ToString(CultureInfo.InvariantCulture)];
+        }
+
+        /// <summary>  
+        /// 根据枚举类型得到其所有的 值 与 枚举定义Description属性 的集合  
+        /// </summary>  
+        /// <param name="enumType"></param>  
+        /// <returns></returns>  
+        public static NameValueCollection GetNVCFromEnumValue(Type enumType)
+        {
+            var nvc = new NameValueCollection();
+            Type typeDescription = typeof(DescriptionAttribute);
+            FieldInfo[] fields = enumType.GetFields();
+            foreach (FieldInfo field in fields)
+            {
+                if (field.FieldType.IsEnum)
+                {
+                    string strValue = ((int)enumType.InvokeMember(field.Name, BindingFlags.GetField, null, null, null)).ToString(CultureInfo.InvariantCulture);
+                    object[] arr = field.GetCustomAttributes(typeDescription, true);
+                    string strText;
+                    if (arr.Length > 0)
+                    {
+                        var aa = (DescriptionAttribute)arr[0];
+                        strText = aa.Description;
+                    }
+                    else
+                    {
+                        strText = "";
+                    }
+                    nvc.Add(strValue, strText);
+                }
+            }
+            return nvc;
+        }
+    }//end of class  
+}
---
+++ mymvcpj/MvcApplication1/Controllers/EnumTestController.cs
@@ -0,0 +1,43 @@
+﻿using MvcApplication1.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class EnumTestController : Controller
+    {
+        //
+        // GET: /EnumTest/
+        public enum DiseaseDiagnosis
+        {
+            [Description("精神分裂症")]
+            Schizophrenia = 1,
+            [Description("双相情感障碍")]
+            BipolarDisorder = 2,
+            [Description("抑郁症")]
+            Depression = 3,
+            [Description("强迫症")]
+            Obsession = 4,
+            [Description("广泛性焦虑障碍")]
+            Anxiety = 5,
+            [Description("ADHD")]
+            Adhd = 6,
+            [Description("神经性贪食症")]
+            BulimiaNervosa = 7,
+            [Description("其他")]
+            Others = 8,
+        }
+
+        public ActionResult Index()
+        {
+            var mm = EnumHelper.GetNVCFromEnumValue(typeof( DiseaseDiagnosis));
+            ViewBag.mmm = mm;
+            return View();
+        }
+
+    }
+}
